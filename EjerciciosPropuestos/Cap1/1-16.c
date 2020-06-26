4509d11bcea7ca81dc47b66ed041d56a764b1980
Diego Mendez Medina <DiegoMendezMedina@github.com>
2020-06-26 15:50:18 -0500

    Actualizando Ejercicios

--- EjerciciosPropuestos/Cap1/1-16.c
+++ EjerciciosPropuestos/Cap1/1-16.c
@@ -5,7 +5,7 @@ int getlinee(char line[], int maxline);
 void copy(char to[], char from[]);
 
 int main(){
-  int len; /* longitf actual de la linea*/
+  int len; /* longitud actual de la linea*/
   int max; /*maxima longitud vista hasta el momento*/
   char line[MAXLINE]; /*linea de la entrada actual*/
   char longest[MAXLINE]; /*Donde se guarda la linea mas larga*/
@@ -17,21 +17,33 @@ int main(){
       copy(longest, line);      
     }
   if(max > 0)
-    printf("%s", longest);
+    printf("Longitud de la cadena mas larga: %d \nCadena mas larga: %s \n", max,longest);
   return 0;
 }
 
 /* getline: lee una linea en s, regresa su longitud. */
 int getlinee(char s[], int lim){
-  int c, i;
-
-  for(i = 0; i < lim-1 && ( c = getchar()) != EOF && c != '\n'; i++)
-    s[i] = c;
-  if(c == '\n'){
-    s[i] = c;
-    i++;
+  int c, i, j;
+  lim = lim - 2;
+  for(i = 0; (c = getchar()) != EOF && c != '\n'; i++)
+    if(i < lim){
+      s[i] = c;
+      j = i;
+    }
+  if(j == lim - 1){
+    s[j] = '\n';
+    j++;
+    s[j] = '\0';
+    if(i > 0)
+      i++;
   }
-  s[i] = '\0';
+  else{
+    if(c == '\n'){
+      s[i] = c;
+      i++;
+    }
+    s[i] = '\0';
+  }
   return i;
 }
 
